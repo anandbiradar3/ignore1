b5c7531b57d344e17121f2cbf94b29e7a9cc1461
anandbiradar3 <125869875+anandbiradar3@users.noreply.github.com>
2023-03-16 18:24:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_s3_bucket" "my_ec2_instance" {
-    ami = "ami-0d81306eddc614a45"
-    instance_type = "t2.micro"
-    count= 3
-    tags = {
+    ami="ami-0d81306eddc614a45"
+    instance_type= "t2.micro"
+    count=3
+    tags= {
         Name ="terraform"
     }
 }
